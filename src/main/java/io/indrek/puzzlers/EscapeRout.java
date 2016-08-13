8edfdef829dcf6aeb8de1016ed1df83674530eb0
Indrek Ots <ots.indrek@gmail.com>
2016-08-13 13:17:42 +0300

    create hello unicode example

---
+++ src/main/java/io/indrek/puzzlers/EscapeRout.java
@@ -0,0 +1,7 @@
+package io.indrek.puzzlers;
+
+public class EscapeRout {
+    public static void main(String[] args) {
+        System.out.println("a\u0022.length() + \u0022b".length());
+    }
+}
---
+++ src/main/java/io/indrek/puzzlers/HelloUnicode.java
@@ -0,0 +1,8 @@
+package io.indrek.puzzlers;
+
+public class HelloUnicode {
+    public static void main(String[] args) {
+        // \u0055 is a Unicode escape for the capital U character (U)
+        System.out.println("Hello \u0055nicode".length());
+    }
+}
