24798cd46bc26065e8259013384ba7a858ddb41a
Indrek Ots <ots.indrek@gmail.com>
2016-08-16 12:17:12 +0300

    show ill formed unicode escape in action

---
+++ src/main/java/io/indrek/puzzlers/IllFormedUnicodeEscape.java
@@ -0,0 +1,8 @@
+package io.indrek.puzzlers;
+
+public class IllFormedUnicodeEscape {
+    public static void main(String[] args) {
+        // user data is read from C:\data\users\profile
+        System.out.println("User data");
+    }
+}
